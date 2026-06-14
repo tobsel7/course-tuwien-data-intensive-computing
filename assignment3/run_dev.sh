7d90e3442f49b88282a0cfee11d1bc8517c09daf
Tobias Haider <tobias.haider99@hotmail.com>
2026-06-14 18:59:46 +0200

    set environment variables

--- assignment3/run_dev.sh
+++ assignment3/run_dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
 
 ENDPOINT="http://localhost:4566"
 AWS="aws --endpoint-url=$ENDPOINT"
